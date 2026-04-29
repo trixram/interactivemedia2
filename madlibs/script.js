044a9694e3a35ad730bac0a923f79233c2421bda
Trixibelle Ramirez <tpramirez@ucdavis.edu>
2026-04-29 13:12:43 -0700

    added new styles

--- madlibs/script.js
+++ madlibs/script.js
@@ -4,9 +4,13 @@
 
     const madLibs = document.querySelector('#madLibs');
     const results = document.querySelector('#results');
+    const story = document.querySelector('.story');
 
     madLibs.addEventListener('submit', function(event){
         event.preventDefault();
+
+        const target = document.querySelector('.story');
+
 
         const adj = document.querySelector('#adj').value;
         const adverb2 = document.querySelector('#adverb2').value;
@@ -22,7 +26,10 @@
 
         const food = document.querySelector('#food').value;
 
-        const myText = `<label for="adj">Captain Whiskers is a <b>${adj}</b></label>
+        const myText = `
+
+        
+        <label for="adj">Captain Whiskers is a <b>${adj}</b></label>
 
 
                     <span><b>${noun1}</b>-powered ship through the stars.</span>
@@ -36,7 +43,7 @@
                     <br><br>
                         <label>Without hesitation, Captain Whiskers moved <b>${adverb2}</b></label>
 
-                    </select>
+                  
                     <span>through asteroid fields and glowing galaxies. As Captain Whiskers zooms through space, he weave through the galaxies swirling neon gas clouds, passing through Saturn and Mars. As Captain Whiskers drives his ship, the ship begins to beep faster signaling how close he is to the lost kitties in <b>${planet2}</b>.</span>
 
                     <span> As captain Whiskers got closer, he saw <b>${noun2}</b></span>
@@ -57,10 +64,13 @@
                     <span> And just like that, all the kitties were saved, and Captain Whiskers saved the day! </span>
                     
                     <footer class="mad-footer">
-                    <a href="../index.html">PORTAL PAGE</a>
-                    <a href="madlibs.html">RETURN</a>
+                    
+                    <a href="madlibs-play.html"><img src="images/replay_30dp_714071_FILL0_wght400_GRAD0_opsz24.png" alt="Icon of a replay"></a>
+                    <a href="../index.html"><img src="images/home_30dp_714071_FILL0_wght400_GRAD0_opsz24.png" alt="Icon of a home"></a>
+                    <a href="&#116;&#112;&#114;&#097;&#109;&#105;&#114;&#101;&#122;&#064;&#117;&#099;&#100;&#097;&#118;&#105;&#115;&#046;&#101;&#100;&#117;"><img src="images/mail_30dp_714071_FILL0_wght400_GRAD0_opsz24.png"></a>
                     </footer>`
 
         madLibs.innerHTML = myText; 
+        story.scrollTop= 0; 
     })
 })();
